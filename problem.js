8ea9ad31f1aa34491a9f8c2dfbd10918a816b847
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-04-15 20:45:25 +0600

    solve push

---
+++ problem.js
@@ -0,0 +1,11 @@
+// Input: 121 → Output: true  
+// Input: 123 → Output: false
+
+
+function isReversed(num) {
+    let str= num.toString();
+    let isReversed = str.split('').reverse().join('');
+    return str === isReversed
+}
+console.log(isReversed(121));
+console.log(isReversed(123));

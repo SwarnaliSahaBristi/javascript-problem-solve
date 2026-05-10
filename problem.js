bee0d8ad32b002a2f3add46f9661e127e8916fb4
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2026-05-10 21:03:49 +0600

    problem added

--- problem.js
+++ problem.js
@@ -11,3 +11,7 @@ function isReversed(num) {
 // console.log(isReversed(123));
 
 
+// Given a sorted array, remove duplicates in-place such that each element appears only once.
+// Return the number of unique elements.
+// Input: [1,1,2]
+// Output: 2
